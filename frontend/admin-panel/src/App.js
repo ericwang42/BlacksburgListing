1afad5f235152e238eeb73d09737cc6bb15e357a
Eric Wang <ericwang@Erics-MacBook-Pro.local>
2024-04-16 21:46:15 -0400

    please

---
+++ frontend/admin-panel/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import './components/adminPanel.js';
+import AdminPanel from './components/adminPanel.js';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+
+function App() {
+  return (
+    <AdminPanel />
+  );
+}
+
+export default App;
